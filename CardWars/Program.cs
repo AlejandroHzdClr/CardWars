8626b55ba2188dc982a9378d05802ca1a63afe78
AlejandroHzdClr <alehdzclr@gmail.com>
2026-04-28 16:56:11 +0200

    SFML principios

--- CardWars/Program.cs
+++ CardWars/Program.cs
@@ -1,43 +1,23 @@
-﻿using CardWars.Cards;
-using CardWars.Player;
-
-namespace CardWars;
+﻿using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
 
 class Program
 {
-    public static int Main()
+    static void Main()
     {
-        /*Card luffy = new Card("Monkey D. Luffy", 5000, 5, 1000);
-        Card zoro = new Card("Roronoa Zoro", 5000, 3, 1000);
-        Card sanji = new Card("Sanji", 4000, 2, 2000);
-        Card nami = new Card("Nami", 1000, 1, 1000);
-        Card usopp = new Card("Usopp", 2000, 2, 2000);
+        var window = new RenderWindow(
+            new VideoMode(new Vector2u(800, 600)),
+            "SFML en C#"
+        );
 
-        List<Card> cartasDePrueba = new List<Card>()
+        window.Closed += (_, __) => window.Close();
+
+        while (window.IsOpen)
         {
-            luffy, zoro, sanji, nami, usopp
-        };
-
-        Deck deckPrueba = new Deck(cartasDePrueba);
-        Hand handPrueba = new Hand(cartasDePrueba);
-        
-        //deckPrueba.ShowCards();
-
-        Player.Player alejandro = new Player.Player("Alejandro", 5, 5000, deckPrueba, handPrueba);
-        Player.Player eduardo = new Player.Player("Eduardo", 5, 5000, deckPrueba, handPrueba);
-        
-        alejandro.PlayerDeck.Shuffle();
-        alejandro.PlayerDeck.ShowCards();
-        
-        Console.WriteLine('\n');
-        
-        eduardo.PlayerDeck.Shuffle();
-        eduardo.PlayerDeck.ShowCards();
-        
-        alejandro.PlayerDeck.CardDeck[0].OnAttack(eduardo.PlayerDeck.CardDeck[0]);*/
-        
-        
-        
-        return 0;
+            window.DispatchEvents();
+            window.Clear(Color.Black);
+            window.Display();
+        }
     }
 }
